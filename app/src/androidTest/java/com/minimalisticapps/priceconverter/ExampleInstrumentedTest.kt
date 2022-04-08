27d5311ce0ce86f31e75446bae3d6bac2184a72c
hank.milliken94@protonmail.com <Hank Milliken>
2022-04-08 11:05:30 +0200

    fix forgotten rename

--- app/src/androidTest/java/com/minimalisticapps/priceconverter/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/minimalisticapps/priceconverter/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.codesses.priceconverter", appContext.packageName)
+        assertEquals("com.minimalisticapps.priceconverter", appContext.packageName)
     }
 }
